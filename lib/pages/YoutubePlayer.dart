d66d0cd78f318d63a5177d64cad44d8e72f4b279
wilsonyu111 <wilsonyu009@gmail.com>
2022-01-20 00:24:24 -0800

    YoutubePlayer.dart can now play any youtube link passed into the class. Basic controls are available but they need to be worked on and the aspect ratio will also need to be adjusted based on the device this app is tested on.

--- lib/pages/YoutubePlayer.dart
+++ lib/pages/YoutubePlayer.dart
@@ -1,1 +1,45 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
+// ignore: must_be_immutable
+class YoutubeTrailer extends StatefulWidget {
+  String urlLink;
+
+  YoutubeTrailer(this.urlLink, {Key? key}) : super(key: key);
+
+  @override
+  _YoutubeTrailerState createState() => _YoutubeTrailerState();
+}
+
+class _YoutubeTrailerState extends State<YoutubeTrailer> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    _controller = YoutubePlayerController(
+        initialVideoId:
+            YoutubePlayerController.convertUrlToId(widget.urlLink!)!,
+        params: const YoutubePlayerParams(
+          showControls: true,
+          mute: true,
+        ));
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return SizedBox(
+      height: kIsWeb ? screenSize.height / 1.13 : screenSize.height,
+      width: screenSize.width,
+      child: YoutubePlayerControllerProvider(
+        controller: _controller,
+        child: YoutubePlayerIFrame(
+          controller: _controller,
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'VideoPlayer.dart';
+import 'YoutubePlayer.dart';
 
 class ShowDetails extends StatefulWidget {
   final String showId;
 
-  ShowDetails({Key? key, required this.showId}) : super(key: key);
+  const ShowDetails({Key? key, required this.showId}) : super(key: key);
 
   @override
   _ShowDetailsState createState() => _ShowDetailsState();
@@ -13,11 +16,22 @@ class _ShowDetailsState extends State<ShowDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text(
-          'showId: ${widget.showId}',
-        ),
+      appBar: AppBar(
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Text(
+            widget.showId,
+          ),
+          Container(
+            alignment: Alignment.center,
+            height: 600,
+            width: 400,
+            child:
+                YoutubeTrailer("https://www.youtube.com/watch?v=JfVOs4VSpmA"),
+          ),
+        ],
       ),
     );
   }
