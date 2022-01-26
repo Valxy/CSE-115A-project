ab051754130504a19bff1f1b69ceda09617ee668
Jesse Chan <jchen304@ucsc.edu>
2022-01-25 17:38:31 -0800

    Rework entry, add fade-in animation for tabs, and add "Browse" tab file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'tabs/browse.dart';
 import 'tabs/explore.dart';
 import 'tabs/search.dart';
 
@@ -7,14 +8,28 @@ void main() {
   runApp(TMDBApp());
 }
 
-const List<Widget> _pages = <Widget>[
-  ExploreTab(),
-  SearchTab(),
-  Center(
-    child: Text(
-      'TBD: BrowseTab',
+const List<List<Widget>> _tabs = [
+  [
+    NavigationDestination(
+      icon: Icon(Icons.explore),
+      label: 'Explore',
     ),
-  ),
+    ExploreTab()
+  ],
+  [
+    NavigationDestination(
+      icon: Icon(Icons.search),
+      label: 'Search',
+    ),
+    SearchTab()
+  ],
+  [
+    NavigationDestination(
+      icon: Icon(Icons.view_list),
+      label: 'Browse',
+    ),
+    BrowseTab()
+  ],
 ];
 
 class TMDBApp extends StatefulWidget {
@@ -22,13 +37,31 @@ class TMDBApp extends StatefulWidget {
   _TMDBAppState createState() => _TMDBAppState();
 }
 
-class _TMDBAppState extends State<TMDBApp> {
-  int _currentPageIndex = 0;
+class _TMDBAppState extends State<TMDBApp> with SingleTickerProviderStateMixin {
+  int _currentTabIndex = 0;
+
+  late AnimationController _animationController;
+  late Animation _animation;
 
   void _setIndex(int i) {
     setState(() {
-      _currentPageIndex = i;
+      _currentTabIndex = i;
     });
+
+    _animationController.forward(from: 0.0);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 100));
+
+    _animation = Tween<double>(begin: 0, end: 1.0).animate(CurvedAnimation(
+        parent: _animationController, curve: Curves.easeInCubic));
+
+    _animationController.forward();
   }
 
   @override
@@ -37,25 +70,20 @@ class _TMDBAppState extends State<TMDBApp> {
       title: 'The Movie Database',
       theme: ThemeData.light(),
       home: Scaffold(
-        body: _pages.elementAt(_currentPageIndex),
+        body: AnimatedBuilder(
+          animation: _animationController,
+          builder: (_, __) {
+            return Opacity(
+              opacity: _animation.value,
+              child: _tabs.elementAt(_currentTabIndex)[1],
+            );
+          },
+        ),
         bottomNavigationBar: NavigationBar(
-          selectedIndex: _currentPageIndex,
+          selectedIndex: _currentTabIndex,
           labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
           onDestinationSelected: _setIndex,
-          destinations: const [
-            NavigationDestination(
-              icon: Icon(Icons.explore),
-              label: 'Explore',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.search),
-              label: 'Search',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.view_list),
-              label: 'Browse',
-            ),
-          ],
+          destinations: List.of(_tabs.map((e) => e.elementAt(0))),
         ),
       ),
     );
---
+++ lib/tabs/browse.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BrowseTab extends StatefulWidget {
+  const BrowseTab({Key? key}) : super(key: key);
+
+  @override
+  _BrowseTabState createState() => _BrowseTabState();
+}
+
+class _BrowseTabState extends State<BrowseTab> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'TBD: BrowseTab',
+      ),
+    );
+  }
+}
