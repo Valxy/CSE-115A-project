f3cbef5ad3275313043354fd2215be3692265d2c
wilsonyu111 <wilsonyu009@gmail.com>
2022-01-31 10:24:45 -0800

    now the page will start with an empty list and then it'll be populated with results after a search query. I'll continue working on it once the backend is implemented with the query functionality.

--- lib/tabs/search.dart
+++ lib/tabs/search.dart
@@ -9,46 +9,19 @@ class SearchTab extends StatefulWidget {
 }
 
 class _SearchTabState extends State<SearchTab> {
-  bool freshPage = true;
-  List<String> someList = ["f", "e", "d", "c", "b", "a"];
+  List<String> someList = [];
+
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    const title = 'Floating App Bar';
-
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        resizeToAvoidBottomInset:
-            false, // prevents the keyboard resizing the search bar
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              // collapsedHeight: 10.0, does not work for some reason...
-              floating: true,
-              // backgroundColor: Color.fromARGB(0, 20, 130, 219),
-              // shadowColor: Colors.transparent,
-              // foregroundColor: Color.fromARGB(0, 20, 130, 219),
-              expandedHeight: 130,
-              // a work around for an overflow issue
-              // works by adding an empty box below the search bar
-              bottom: const PreferredSize(
-                preferredSize: Size.fromHeight(60.0),
-                child: Text(''),
-              ),
-              flexibleSpace: Center(
-                child: TextInput(someList),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) => ListTile(title: Text(someList[index])),
-                childCount: someList.length,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return TextInput();
   }
 }
--- lib/tabs/text_input.dart
+++ lib/tabs/text_input.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 
 // Define a custom Form widget.
 class TextInput extends StatefulWidget {
-  late List<String> someList;
-
-  TextInput(List<String> someList, {Key? key}) : super(key: key) {
-    this.someList = someList;
-  }
+  TextInput({Key? key}) : super(key: key);
 
   @override
   _TextInputState createState() => _TextInputState();
@@ -15,8 +11,8 @@ class TextInput extends StatefulWidget {
 // Define a corresponding State class.
 // This class holds the data related to the Form.
 class _TextInputState extends State<TextInput> {
-  // Create a text controller and use it to retrieve the current value
-  // of the TextField.
+  List<String> queryResult = [];
+
   final myController = TextEditingController();
 
   @override
@@ -28,47 +24,81 @@ class _TextInputState extends State<TextInput> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          width: 400,
-          height: 50,
-          margin:
-              const EdgeInsets.only(left: 20, right: 20, top: 50, bottom: 0),
-          // color: const Color.fromARGB(255, 23, 163, 218),
-          decoration: const BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: Colors.transparent,
-          ),
-          child: TextField(
-            controller: myController,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'search movies and tv shows',
+    const title = 'Floating App Bar';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        resizeToAvoidBottomInset:
+            false, // prevents the keyboard resizing the search bar
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              // collapsedHeight: 10.0, does not work for some reason...
+              floating: true,
+              // backgroundColor: Color.fromARGB(0, 20, 130, 219),
+              // shadowColor: Colors.transparent,
+              // foregroundColor: Color.fromARGB(0, 20, 130, 219),
+              expandedHeight: 130,
+              // a work around for an overflow issue
+              // works by adding an empty box below the search bar
+              bottom: const PreferredSize(
+                preferredSize: Size.fromHeight(60.0),
+                child: Text(''),
+              ),
+              flexibleSpace: Center(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: 400,
+                      height: 50,
+                      margin: const EdgeInsets.only(
+                          left: 20, right: 20, top: 50, bottom: 0),
+                      // color: const Color.fromARGB(255, 23, 163, 218),
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        color: Colors.transparent,
+                      ),
+                      child: TextField(
+                        controller: myController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'search movies and tv shows',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      //margin: const EdgeInsets.only(left: 20, right: 20, top: 0, bottom: 0),
+                      width: 60,
+                      height: 30,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          primary: Colors.red,
+                          onSurface: Colors.red,
+                        ),
+                        onPressed: () {
+                          for (int i = 0; i < 30; i++) {
+                            setState(() {
+                              queryResult.add("${myController.text} $i");
+                            });
+                          }
+                        },
+                        child: const Text('Search'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ),
-        Container(
-          //margin: const EdgeInsets.only(left: 20, right: 20, top: 0, bottom: 0),
-          width: 60,
-          height: 30,
-          child: TextButton(
-            style: TextButton.styleFrom(
-              primary: Colors.red,
-              onSurface: Colors.red,
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => ListTile(title: Text(queryResult[index])),
+                childCount: queryResult.length,
+              ),
             ),
-            onPressed: () {
-              for (int i = 0; i < widget.someList.length; i++) {
-                widget.someList[i] += "a";
-                //print(widget.someList);
-              }
-              print(myController.text);
-              print(widget.someList);
-            },
-            child: const Text('Search'),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
