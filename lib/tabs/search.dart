c0519fbc6a4700a5abc545db8363f7ae8e019763
wilsonyu111 <wilsonyu009@gmail.com>
2022-02-05 21:22:39 -0800

    search page is completely overhauled in terms of visual and functionality. The search functionality will need to be worked on further The suggestion in the result page can now be clicked and go to the detailed page.

--- lib/tabs/search.dart
+++ lib/tabs/search.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../widgets/TextInput.dart';
+import '../models/tmdb_api_wrapper.dart';
+import '../pages/details.dart';
 
 class SearchTab extends StatefulWidget {
   const SearchTab({Key? key}) : super(key: key);
@@ -10,19 +10,126 @@ class SearchTab extends StatefulWidget {
 }
 
 class _SearchTabState extends State<SearchTab> {
-  List<String> someList = [];
-
-  final myController = TextEditingController();
-
   @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    myController.dispose();
-    super.dispose();
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("search movies and tv shows"),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              // search button
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: SearchItem());
+              },
+            )
+          ],
+          backgroundColor: Colors.purple,
+        ),
+        body: Container(
+          color: Colors.black,
+          child: const Center(
+            child: Text(
+              'test wrapper',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 64,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      );
+}
+
+class SearchItem extends SearchDelegate<String> {
+  final popularMovies = ["test1", "test2", "test3", "test4"];
+  @override
+  // clear button
+  // this function handles the X button on the top right corner
+  // it clears the search query or closes the search bar if search bar is empty
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          if (query.isEmpty) {
+            close(context, "");
+          } else {
+            query = "";
+          }
+        },
+      )
+    ];
   }
 
+  // backward button
+  // this function is similar to the handle X button function
+  // it closes the search bard regardless the content in the serach bar
   @override
-  Widget build(BuildContext context) {
-    return TextInput();
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, "");
+      },
+    );
+  }
+
+  // this function will build the query result and display it in a list
+  // this will need to be impelmented as a list later
+  // this only show one tile for now
+  @override
+  Widget buildResults(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.local_movies, size: 120),
+          const SizedBox(
+            height: 48,
+          ),
+          Text(
+            query,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 64,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  // the plan for this function is to show some suggested movies when
+  // the serach bar is open, this will be implemented later
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestions = popularMovies;
+    return buildSuggestionsSuccess(suggestions);
+  }
+
+  Widget buildSuggestionsSuccess(List<String> suggestions) {
+    return ListView.builder(
+        itemCount: suggestions.length,
+        itemBuilder: (context, index) {
+          final suggestion = suggestions[index];
+          return ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) =>
+                      const ShowDetails(showId: "634649"),
+                  fullscreenDialog: true,
+                ),
+              );
+            },
+            leading: const Icon(Icons.local_movies_outlined),
+            title: Text(suggestion),
+          );
+        });
   }
 }
