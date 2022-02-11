1e7773e5cfd0c5885cf3fa05d3ee559070e5b547
Jesse Chan <jchen304@ucsc.edu>
2022-02-10 22:05:16 -0800

    details: implement the release date, runtime, rating and crew
    Closes: #4, #5, #20, #21

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../models/tmdb_api_wrapper.dart';
@@ -54,15 +55,15 @@ class _ShowDetailsState extends State<ShowDetails> {
             child: Column(
               children: [
                 Container(
-                  margin: const EdgeInsets.symmetric(vertical: 20.0),
-                  height: 216.0,
-                  child: ListView(
-                    // This next line does the trick.
-                    scrollDirection: Axis.horizontal,
-                    children: <Widget>[
+                  child: CarouselSlider(
+                    options: CarouselOptions(
+                      disableCenter: true,
+                      viewportFraction: 1,
+                    ),
+                    items: <Widget>[
                       Container(
                         width: 384.0,
-                        child: posters[0],
+                        color: Colors.red,
                       ),
                       Container(
                         width: 384.0,
@@ -84,24 +85,92 @@ class _ShowDetailsState extends State<ShowDetails> {
                   ),
                 ),
                 Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      FutureBuilder<Movie>(
-                        future: movieDetails,
-                        builder:
-                            (BuildContext ctx, AsyncSnapshot<Movie> snapshot) {
-                          if (snapshot.hasData) {
-                            return ListTile(
-                              title: Text(snapshot.data?.title ?? ""),
-                              subtitle: Text(snapshot.data?.overview ?? ""),
-                            );
-                          }
-
-                          return CircularProgressIndicator();
-                        },
-                      )
-                    ],
+                  child: Container(
+                    height: 240,
+                    padding: const EdgeInsets.all(0),
+                    child: Row(children: [
+                      posters[0],
+                      const Spacer(
+                        flex: 1,
+                      ),
+                      Expanded(
+                        flex: 22,
+                        child: Container(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                snapshot.data?.title ?? "",
+                                style: const TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              const Text(
+                                "",
+                              ),
+                              Wrap(
+                                spacing: 18,
+                                children: [
+                                  Text(
+                                    snapshot.data!.releaseDate.split("-")[0],
+                                  ),
+                                  const Text(
+                                    "PG-13",
+                                  ),
+                                  Text(
+                                    "${snapshot.data!.runtime! ~/ 60}h",
+                                  )
+                                ],
+                              ),
+                              const Text(
+                                "",
+                              ),
+                              Text(snapshot.data?.overview ?? "",
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                  )),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const Spacer(
+                        flex: 1,
+                      ),
+                    ]),
+                  ),
+                ),
+                Card(
+                  child: Container(
+                    height: 150,
+                    padding: const EdgeInsets.all(0),
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: snapshot.data!.crew
+                          .map(
+                            (e) => Column(
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: CircleAvatar(
+                                    radius: 50.0,
+                                    child: Text(e.name.split(" ")[0][0]),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 100,
+                                  child: Center(
+                                    child: Text(
+                                      e.name,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                          .toList(),
+                    ),
                   ),
                 ),
               ],
