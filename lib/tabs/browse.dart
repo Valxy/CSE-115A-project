4398a422fa7c0a65d49a9da403128c6e3fd8470f
Jesse Chan <jchen304@ucsc.edu>
2022-02-18 15:14:53 -0800

    browse: increase code reuse

--- lib/tabs/browse.dart
+++ lib/tabs/browse.dart
@@ -18,344 +18,49 @@ class _BrowseTabState extends State<BrowseTab> {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Action & Adventure",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Animation",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Comedy",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Crime",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Documentary",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Drama",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Family",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Kids",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Fantasy",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("History",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Horror",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Music",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Mystery",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("News",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Romance",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Science Fiction",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Talk",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("TV Movie",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Thriller",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("War & Politics",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: 100.0,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.red, width: 0.3)),
-              child: Center(
-                child: Text("Western",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    )),
-              ),
-            ),
-          ],
+          children: [
+            "Action & Adventure",
+            "Animation",
+            "Comedy",
+            "Crime",
+            "Documentary",
+            "Drama",
+            "Family",
+            "Kids",
+            "Fantasy",
+            "History",
+            "Horror",
+            "Music",
+            "Mystery",
+            "News",
+            "Romance",
+            "Science Fiction",
+            "Talk",
+            "TV Movie",
+            "Thriller",
+            "War & Politics",
+            "Western",
+          ]
+              .map(
+                (e) => Container(
+                  margin: const EdgeInsets.all(5.0),
+                  height: 100.0,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(4.0)),
+                      color: Colors.blue,
+                      border: Border.all(color: Colors.red, width: 0.3)),
+                  child: Center(
+                    child: Text(e,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 30,
+                        )),
+                  ),
+                ),
+              )
+              .toList(),
         ),
       ),
     );
