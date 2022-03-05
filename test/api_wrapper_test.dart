f7c4d8f92522676f2c820dc0422144933ddca156
jferre25 <52797500+jferre25@users.noreply.github.com>
2022-03-04 16:59:49 -0800

    changed to use dart's testing library
    also added tests for the genre getters

--- test/api_wrapper_test.dart
+++ test/api_wrapper_test.dart
@@ -140,219 +140,128 @@ Future<bool> _testSearch(String query) async {
   try {
     List<dynamic> searchResults = await TmdbApiWrapper().search(query: query);
 
-    expect(searchResults, isNotEmpty);
+    // expect(searchResults, isNotEmpty);
   } catch (e) {
     return false;
   }
   return true;
 }
 
-// Future<void> _testImageCaching() async {
-//   List<MinimizedMovie> movie = await TmdbApiWrapper().getPopularMovies(1);
-//   Widget image =
-//       await TmdbApiWrapper().getImage(imagePath: movie[0].posterPath);
-
-//   expect(image, isNotEmpty);
-// }
+Future<bool> _testGenreGetters() async {
+  for (var k in TmdbApiWrapper().genreDictionary.keys) {
+    try {
+      TmdbApiWrapper().getMovieListFromGenreId(genreId: TmdbApiWrapper().genreDictionary[k]);
+    } on PageNotFoundException {
+      // expect a page not found exception for genres
+      // that are specific to tv or movie. Simply continue
+      continue;
+    } catch (e) {
+      return false;
+    }
+    try {
+      TmdbApiWrapper().getTvListFromGenreId(genreId: TmdbApiWrapper().genreDictionary[k]);
+    } on PageNotFoundException {
+      // expect a page not found exception for genres
+      // that are specific to tv or movie. Simply continue
+      continue;
+    } catch (e) {
+      return false;
+    }
+  }
+  return true;
+}
 
 void main() {
-  test('Get popular movies', _testGetPopularMovies);
-  test('Now playing movies', _testNowPlayingMovies);
-  test('Top rated movies', _testGetTopRatedMovies);
+  test('Get popular movies', () => expect(_testGetPopularMovies(), true));
+  test('Now playing movies', () => expect(_testNowPlayingMovies(), true);
+  test('Top rated movies', () => expect(_testGetTopRatedMovies(), true));
+  test('Latest movie', () => expect(_testGetLatestMovie(), true));
+  MinimizedMovie testMovie = await TmdbApiWrapper().getLatestMovie();
+  List<MinimizedTvShow> shows = await TmdbApiWrapper().getAnimatedTvShows();
+  MinimizedTvShow testShow = shows[0];
+  test('Top rated TV', () => expect(_testGetTopRatedTvShows(), true));
+  test('Now airing TV Shows', () => expect(_testGetNowAiringTvShows(), true));
+  test('On the air TV Shows', () => expect(_testGetOnTheAirTvShows(), true));
+  test('Popular TV Shows', () => expect(_testGetPopularTvShows(), true));
+  test('Testing getImage', () => expect(testGetImageTvAndMovie(testMovie, testShow), true));
+  test('Get details movie', () => expect(_testGetDetailsMovie(testMovie), true));
+  test('Get detail TV Show', () => expect(_testGetDetailsTvShow(testShow), true));
+  test('Test search', () => expect(_testSearch("Charles"), true));
+  test('Test genre getters', () => expect(_testGenreGetters(), true));
+  test('Movie original language data member', () => expect(testMovie.originalLanguage != null, true));
+  test('Movie id data member', () => expect(testMovie.id != null, true));
+  test('Movie popularity data member', () => expect(testMovie.popularity != null, true));
+  test('Movie posterPath data member', () => expect(testMovie.posterPath != null, true));
+  test('Movie releaseDate data member', () => expect(testMovie.releaseDate != null, true));
+  test('Movie overview data member', () => expect(testMovie.overview != null, true));
+  test('Movie original title data member', () => expect(testMovie.originalTitle != null, true));
+  test('Movie title data member', () => expect(testMovie.title != null, true));
+  test('Movie video data member', () => expect(testMovie.video != null, true));
+  test('Movie voteCount data member', () => expect(testMovie.voteCount != null, true));
+  test('Movie genres data member', () => expect(testMovie.genres != null, true));
+  test('Movie status data member', () => expect(testMovie.status != null, true));
+  test('Movie budget data member', () => expect(testMovie.budget != null, true));
+  test('Movie homepage data member', () => expect(testMovie.homepage != null, true));
+  test('Movie revenue data member', () => expect(testMovie.revenue != null, true));
+  test('Movie runtime data member', () => expect(testMovie.runtime != null, true));
+  test('Movie tagline data member', () => expect(testMovie.tagline != null, true));
+  test('Movie spokenLanguages data member', () => expect(testMovie.spokenLanguages != null, true));
+  test('Movie productionCompanies data member', () => expect(testMovie.productionCompanies != null, true));
+  test('Movie productionCountries data member', () => expect(testMovie.productionCountries != null, true));
+  test('Movie cast data member', () => expect(testMovie.cast != null, true));
+  test('Movie crew data member', () => expect(testMovie.crew != null, true));
+  test('Movie poster data member', () => expect(testMovie.poster != null, true));
+  test('Movie reviews data member', () => expect(testMovie.reviews != null, true));
+  test('Movie backdrops data member', () => expect(testMovie.backdrops != null, true));
+  test('Movie recommendations data member', () => expect(testMovie.recommendations != null, true));
+  test('Movie videos data member', () => expect(testMovie.videos != null, true));
+  test('Movie releases data member', () => expect(testMovie.releases != null, true));
+  test('Movie reviews data member', () => expect(testMovie.reviews != null, true));
+  test('TV Show posterPath data member', () => expect(testShow.posterPath != null, true));
+  test('TV Show originalLanguage data member', () => expect(testShow.originalLanguage != null, true));
+  test('TV Show originalName data member', () => expect(testShow.originalName != null, true));
+  test('TV Show originCountries data member', () => expect(testShow.originCountries != null, true));
+  test('TV Show backdropPath data member', () => expect(testShow.backdropPath != null, true));
+  test('TV Show id data member', () => expect(testShow.id != null, true));
+  test('TV Show overview data member', () => expect(testShow.overview != null, true));
+  test('TV Show popularity data member', () => expect(testShow.popularity != null, true));
+  test('TV Show firstAirDate data member', () => expect(testShow.firstAirDate != null, true));
+  test('TV Show name data member', () => expect(testShow.name != null, true));
+  test('TV Show voteAverage data member', () => expect(testShow.voteAverage != null, true));
+  test('TV Show voteCount data member', () => expect(testShow.voteCount != null, true));
+  test('TV Show genres data member', () => expect(testShow.genres != null, true));
+  test('TV Show creators data member', () => expect(testShow.creators != null, true));
+  test('TV Show homepage data member', () => expect(testShow.homepage != null, true));
+  test('TV Show episodeRunTimes data member', () => expect(testShow.episodeRunTimes != null, true));
+  test('TV Show languages data member', () => expect(testShow.languages != null, true));
+  test('TV Show isInProduction data member', () => expect(testShow.isInProduction != null, true));
+  test('TV Show lastAirDate data member', () => expect(testShow.lastAirDate != null, true));
+  test('TV Show lastEpisodeToAir data member', () => expect(testShow.lastEpisodeToAir != null, true));
+  test('TV Show nextEpisodeToAir data member', () => expect(testShow.nextEpisodeToAir != null, true));
+  test('TV Show networks data member', () => expect(testShow.networks != null, true));
+  test('TV Show numberOfEpisodes data member', () => expect(testShow.numberOfEpisodes != null, true));
+  test('TV Show numberOfSeasons data member', () => expect(testShow.numberOfSeasons != null, true));
+  test('TV Show productionCountries data member', () => expect(testShow.productionCountries != null, true));
+  test('TV Show productionCompanies data member', () => expect(testShow.productionCompanies != null, true));
+  test('TV Show seasons data member', () => expect(testShow.seasons != null, true));
+  test('TV Show spokenLanguages data member', () => expect(testShow.spokenLanguages != null, true));
+  test('TV Show status data member', () => expect(testShow.status != null, true));
+  test('TV Show crew data member', () => expect(testShow.crew != null, true));
+  test('TV Show cast data member', () => expect(testShow.cast != null, true));
+  test('TV Show type data member', () => expect(testShow.type != null, true));
+  test('TV Show recommendations data member', () => expect(testShow.recommendations != null, true));
+  test('TV Show videos data member', () => expect(testShow.videos != null, true));
+  test('TV Show reviews data member', () => expect(testShow.reviews != null, true));
+  test('TV Show backdrops data member', () => expect(testShow.backdrops != null, true));
+  test('TV Show poster data member', () => expect(testShow.poster != null, true));
 }
 
-Future<void> testApiWrapper() async {
-  late Movie topRatedMovie;
-  late TvShow topRatedShow;
-  late List<MinimizedMovie> topRatedMovies;
-  late List<MinimizedTvShow> topRatedShows;
-
-  if (!(await _testGetTopRatedMovies())) {
-    return;
-  } else {
-    topRatedMovies = await TmdbApiWrapper().getTopRatedMovies(1);
+  Future<Person> getDetailsPerson({
+    required personId,
+  }) async {
+    final String endpoint =
+        "/person/$personId?api_key=$_apiKey&append_to_response=combined_credits,images";
+    final responseJson = await _helper.get(endpoint);
+    Person person = Person.fromJson(json: responseJson);
+    return person;
   }
-  if (!(await _testGetLatestMovie())) {
-    return;
-  }
-  if (!(await _testGetTopRatedTvShows())) {
-    return;
-  } else {
-    topRatedShows = await TmdbApiWrapper().getTopRatedTvShows(1);
-  }
-  if (!(await _testGetNowAiringTvShows())) {
-    return;
-  }
-  if (!(await _testGetOnTheAirTvShows())) {
-    return;
-  }
-  if (!(await _testGetPopularTvShows())) {
-    return;
-  }
-  if (!(await testGetImageTvAndMovie(topRatedMovies[0], topRatedShows[0]))) {
-    return;
-  }
-  if (!(await _testGetDetailsMovie(topRatedMovies[0]))) {
-    return;
-  } else {
-    topRatedMovie =
-        await TmdbApiWrapper().getDetailsMovie(movieId: topRatedMovies[0].id);
-  }
-  if (!(await _testGetDetailsTvShow(topRatedShows[0]))) {
-    return;
-  } else {
-    topRatedShow =
-        await TmdbApiWrapper().getDetailsTvShow(tvId: topRatedShows[0].id);
-  }
-  if (!(await _testSearch("Charles"))) {
-    return;
-  }
-
-  String dumpString;
-
-  if (kDebugMode) {
-    /// Movie test
-    dumpString = 'Movie data members\n';
-    dumpString = 'original language: ${topRatedMovie.originalLanguage}\n';
-    print(dumpString);
-    dumpString = 'id: ${topRatedMovie.id}\n';
-    print(dumpString);
-    dumpString = 'popularity: ${topRatedMovie.popularity}\n';
-    print(dumpString);
-    dumpString = 'posterPath: ${topRatedMovie.posterPath}\n';
-    print(dumpString);
-    dumpString = 'releaseDate: ${topRatedMovie.releaseDate}\n';
-    print(dumpString);
-    dumpString = 'overview: ${topRatedMovie.overview}\n';
-    print(dumpString);
-    dumpString = 'original title: ${topRatedMovie.originalTitle}\n';
-    print(dumpString);
-    dumpString = 'title: ${topRatedMovie.title}\n';
-    print(dumpString);
-    dumpString = 'video: ${topRatedMovie.video}\n';
-    print(dumpString);
-    dumpString = 'voteCount: ${topRatedMovie.voteCount}\n';
-    print(dumpString);
-    dumpString = 'genres: ${topRatedMovie.genres}\n';
-    print(dumpString);
-    dumpString = 'status: ${topRatedMovie.status}\n';
-    print(dumpString);
-    //removed
-    //dumpString = 'belongsToCollection: ${topRatedMovie.belongsToCollection}\n';
-    //print(dumpString);
-    dumpString = 'budget: ${topRatedMovie.budget}\n';
-    print(dumpString);
-    dumpString = 'homepage: ${topRatedMovie.homepage}\n';
-    print(dumpString);
-    dumpString = 'revenue: ${topRatedMovie.revenue}\n';
-    print(dumpString);
-    dumpString = 'runtime  ${topRatedMovie.runtime}\n';
-    print(dumpString);
-    dumpString = 'tagline  ${topRatedMovie.tagline}\n';
-    print(dumpString);
-    dumpString = 'spokenLanguages: ${topRatedMovie.spokenLanguages}\n';
-    print(dumpString);
-    dumpString = 'productionCompanies: ${topRatedMovie.productionCompanies}\n';
-    print(dumpString);
-    dumpString = 'productionCountries: ${topRatedMovie.productionCountries}\n';
-    print(dumpString);
-    dumpString = 'cast: ${topRatedMovie.cast}\n';
-    print(dumpString);
-    dumpString = 'crew : ${topRatedMovie.crew}\n';
-    print(dumpString);
-    dumpString = 'poster  ${topRatedMovie.poster}\n';
-    print(dumpString);
-    dumpString = 'reviews  ${topRatedMovie.reviews}\n';
-    print(dumpString);
-    dumpString = 'backdrops: ${topRatedMovie.backdrops}\n';
-    print(dumpString);
-    dumpString = 'recommendations  ${topRatedMovie.recommendations}\n';
-    print(dumpString);
-    dumpString = 'videos  ${topRatedMovie.videos}\n';
-    print(dumpString);
-    dumpString = 'releases: ${topRatedMovie.releases}\n';
-    print(dumpString);
-
-    /// TvShow test
-    dumpString = '\nTvShow data members\n';
-    print(dumpString);
-    dumpString = 'originalLanguage: ${topRatedShow.originalLanguage}\n';
-    print(dumpString);
-    dumpString = 'originalName: ${topRatedShow.originalName}\n';
-    print(dumpString);
-    dumpString = 'originCountries: ${topRatedShow.originCountries}\n';
-    print(dumpString);
-    dumpString = 'backdropPath: ${topRatedShow.backdropPath}\n';
-    print(dumpString);
-    dumpString = 'id: ${topRatedShow.id}\n';
-    print(dumpString);
-    dumpString = 'overview: ${topRatedShow.overview}\n';
-    print(dumpString);
-    dumpString = 'popularity: ${topRatedShow.popularity}\n';
-    print(dumpString);
-    dumpString = 'posterPath: ${topRatedShow.posterPath}\n';
-    print(dumpString);
-    dumpString = 'firstAirDate: ${topRatedShow.firstAirDate}\n';
-    print(dumpString);
-    dumpString = 'name: ${topRatedShow.name}\n';
-    print(dumpString);
-    dumpString = 'voteAverage: ${topRatedShow.voteAverage}\n';
-    print(dumpString);
-    dumpString = 'voteCount: ${topRatedShow.voteCount}\n';
-    print(dumpString);
-    dumpString = 'genres: ${topRatedShow.genres}\n';
-    print(dumpString);
-    dumpString = 'creators: ${topRatedShow.creators}\n';
-    print(dumpString);
-    dumpString = 'homepage: ${topRatedShow.homepage}\n';
-    print(dumpString);
-    dumpString = 'episodeRunTimes: ${topRatedShow.episodeRunTimes}\n';
-    print(dumpString);
-    dumpString = 'languages: ${topRatedShow.languages}\n';
-    print(dumpString);
-    dumpString = 'isInProduction: ${topRatedShow.isInProduction}\n';
-    print(dumpString);
-    dumpString = 'lastAirDate: ${topRatedShow.lastAirDate}\n';
-    print(dumpString);
-    dumpString = 'lastEpisodeToAir: ${topRatedShow.lastEpisodeToAir}\n';
-    print(dumpString);
-    dumpString = 'nextEpisodeToAir: ${topRatedShow.nextEpisodeToAir}\n';
-    print(dumpString);
-    dumpString = 'networks: ${topRatedShow.networks}\n';
-    print(dumpString);
-    dumpString = 'numberOfEpisodes: ${topRatedShow.numberOfEpisodes}\n';
-    print(dumpString);
-    dumpString = 'numberOfSeasons: ${topRatedShow.numberOfSeasons}\n';
-    print(dumpString);
-    dumpString = 'productionCountries: ${topRatedShow.productionCountries}\n';
-    print(dumpString);
-    dumpString = 'productionCompanies: ${topRatedShow.productionCompanies}\n';
-    print(dumpString);
-    dumpString = 'seasons: ${topRatedShow.seasons}\n';
-    print(dumpString);
-    dumpString = 'spokenLanguages: ${topRatedShow.spokenLanguages}\n';
-    print(dumpString);
-    dumpString = 'status: ${topRatedShow.status}\n';
-    print(dumpString);
-    dumpString = 'type: ${topRatedShow.type}\n';
-    print(dumpString);
-    dumpString = 'crew: ${topRatedShow.crew}\n';
-    print(dumpString);
-    dumpString = 'cast: ${topRatedShow.cast}\n';
-    print(dumpString);
-    dumpString = 'recommendations: ${topRatedShow.recommendations}\n';
-    print(dumpString);
-    dumpString = 'videos: ${topRatedShow.videos}\n';
-    print(dumpString);
-    dumpString = 'reviews: ${topRatedShow.reviews}\n';
-    print(dumpString);
-    dumpString = 'backdrops: ${topRatedShow.backdrops}\n';
-    print(dumpString);
-    dumpString = 'poster: ${topRatedShow.poster}\n';
-    print(dumpString);
-  }
-  /*
-  for (var i = 0; i < something.length; i++) {}
-  for (var i = 0; i < 20; i++) {}
-  */
-}
